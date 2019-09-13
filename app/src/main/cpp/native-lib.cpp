3b787c0ed4c4d8568ffd8409d7f15d369b33d847
Joseph Kandi <joseph@apps4u.co.za>
2019-09-13 19:08:06 +0200

    Check for root routine

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,7 +1,47 @@
 #include <jni.h>
 #include <string>
+#include <sys/stat.h>
+
+using namespace std;
+
+const string BINARY_SU = "su";
+const string BINARY_BUSYBOX = "busybox";
+const string suPaths[] = {
+    "/data/local/",
+    "/data/local/bin/",
+    "/data/local/xbin/",
+    "/sbin/",
+    "/su/bin/",
+    "/system/bin/",
+    "/system/bin/.ext/",
+    "/system/bin/failsafe/",
+    "/system/sd/xbin/",
+    "/system/usr/we-need-root/",
+    "/system/xbin/",
+    "/cache/",
+    "/data/",
+    "/dev/"
+};
+
+
+bool fileExists(const string &fileName) {
+    struct stat buf;
+    return (stat(fileName.c_str(), &buf) == 0);
+}
+
+bool binaryExists(const string &binary) {
+    for (int i = 0; i < sizeof(suPaths)/ sizeof(*suPaths) ; ++i) {
+        const string fullPath = suPaths[i] + binary;
+        if(fileExists(fullPath)){
+            return true;
+        }
+    }
+    return false;
+}
 
 extern "C" JNIEXPORT jboolean JNICALL Java_com_peruzal_weather_helpers_RootUtils_isRooted(JNIEnv *env, jclass obj){
-    bool isRooted = true;
-    return isRooted;
+    return binaryExists(BINARY_SU) || binaryExists(BINARY_BUSYBOX);
 }
+
+
+
